191a61036bba15891a5e96c2461b9155702d64d9
Erick Capilla <erickpoblano25@gmail.com>
2024-08-25 19:36:00 -0600

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,54 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["apple-touc-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "SIGET",
+        short_name: "siget",
+        description:
+          "Sistema de Gestión del proceso de titulación para la Facultad de ciencias básicas, ingeniería y tecnología de la universidad autónoma de Tlaxcala",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "apple touch icon",
+          },
+          {
+            src: "/maskable_icon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        theme_color: "#610726",
+        background_color: "#FEFCEE",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+      },
+    }),
   ],
 });
